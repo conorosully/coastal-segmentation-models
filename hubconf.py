41c2d74fefe9060b5c214c1aed612f6d5b9d026a
conorosully <conorosullivan@conor.local>
2025-11-28 14:57:13 +0800

    Changed to load state_dict instead of entire model

--- hubconf.py
+++ hubconf.py
@@ -1,21 +1,24 @@
 dependencies = ['torch', 'torchvision', 'huggingface_hub']
-import network as unet
+from network import U_Net
 from huggingface_hub import hf_hub_download
 import torch
 import os
 
 def finetuned_lics():
 
-    model_path = hf_hub_download(repo_id="a-data-odyssey/coastal-image-segmentation", 
-                             filename="LICS_FINETUNE_26JUL24.pth",)
 
+    model_path = hf_hub_download(repo_id="conorosullyDS/coastal-segmentation-models", 
+                                filename="LICS_FINETUNE_26JUL24.pth",)
+
+                
     return get_model(model_path)
 
 def get_model(model_path):
 
-   # model = unet.U_Net()
+    model = U_Net()
 
-    model = torch.load(model_path, map_location=torch.device('cpu'))
+    state_dict = torch.load(model_path, map_location=torch.device('cpu')) 
+    model.load_state_dict(state_dict)
     model.eval()
 
     return model
